3a7bba7a0d1c3fc5c6bd6c24a8eef888692a6800
Joshua Parker <joshua@joshuaparker.dev>
2026-04-28 06:48:48 -0700

    In case of errors.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- src/Core/helpers.php
+++ src/Core/helpers.php
@@ -13,7 +13,7 @@ if (! function_exists('phpb_e')) {
      */
     function phpb_e($value, $doubleEncode = true)
     {
-        return htmlspecialchars($value, ENT_QUOTES, 'UTF-8', $doubleEncode);
+        return htmlspecialchars($value ?? '', ENT_QUOTES, 'UTF-8', $doubleEncode);
     }
 }
 
